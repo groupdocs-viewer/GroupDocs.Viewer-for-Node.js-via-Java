9559a980e5f6d5005dd51449f48a7d55317036d6
RakhimAimaganbetov <rakhim.aimaganbetov@aspose.com>
2023-12-20 15:30:22 +0500

    Pass TimeZone object

--- Examples/AdvancedUsage/Rendering/RenderingOptionsByDocumentType/RenderingEmailMessages/dateTimeFormatAndTimeZoneOffset.js
+++ Examples/AdvancedUsage/Rendering/RenderingOptionsByDocumentType/RenderingEmailMessages/dateTimeFormatAndTimeZoneOffset.js
@@ -1,11 +1,15 @@
+const java = require('java')
+
 async function dateTimeFormatAndTimeZoneOffset(groupdocs, inputFilePath) {
   const viewer = new groupdocs.viewer.Viewer(inputFilePath);
   const outputPath = `${groupdocs.outputFolder}/dateTimeFormatAndTimeZoneOffset.html`;
 
+  const timeOffset = java.import("java.util.TimeZone").getTimeZone("GMT+180")
+
   const viewOptions =
     groupdocs.viewer.HtmlViewOptions.forEmbeddedResources(outputPath);
   viewOptions.getEmailOptions().setDateTimeFormat("MM d yyyy HH:mm tt zzz");
-  viewOptions.getEmailOptions().setTimeZoneOffset(180); // SET TIME OFFSET IN MINUTES
+  viewOptions.getEmailOptions().setTimeZoneOffset(timeOffset); // SET TIME OFFSET IN MINUTES
 
   const view = viewer.view(viewOptions);
   console.log(
